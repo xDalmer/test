65370af915655553455b7f2655e965766d53078f
xDalmer <serge@nasekom.ru>
2016-12-18 13:33:28 +0300

    DecorateProcess

--- coffee.php
+++ coffee.php
@@ -8,3 +8,16 @@ class Coffee{
 abstract class CoffeMaker{
 	abstract public function process (Coffee $coffee);
 }
+
+class MainProcess extends CoffeeMaker {
+	function process(Coffee $coffee){
+		echo __CLASS__, " Всего {$coffee->price}<br />";
+	}
+}
+
+abstract class DecorateProcess extends CoffeeMaker {
+	protected $processmaker;
+	function __construct(CoffeeMaker $maker){
+		$this->processmaker = $maker;
+	}
+}
